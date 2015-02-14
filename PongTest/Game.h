b0970f0c914536eff27fedd9d8b00f576a194878
Rohini Carlson <it-hogskolan@it-hogskolans-MacBook-Pro-96.local>
2015-02-14 11:08:55 +0100

    Added sound and continued to work on game logic.

--- PongTest/Game.h
+++ PongTest/Game.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AVFoundation/AVAudioPlayer.h"
 
 @interface Game : UIViewController
 
--- PongTest/Game.m
+++ PongTest/Game.m
@@ -28,8 +28,12 @@
 
 @property (nonatomic) NSTimer *timer;
 
-@property (nonatomic) int X;
-@property (nonatomic) int Y;
+@property (nonatomic, retain) AVAudioPlayer *audioPlayer;
+
+@property (nonatomic) float screenHeight;
+@property (nonatomic) float screenWidth;
+@property (nonatomic) float X;
+@property (nonatomic) float Y;
 @property (nonatomic) int AIScore;
 @property (nonatomic) int HumanScore;
 
@@ -37,47 +41,80 @@
 
 @implementation Game
 
+-(void) placeObjects{
+    //self.ball.center = self.view.center;
+    self.ball.center = CGPointMake(self.screenWidth/2.0f, self.screenHeight/2.0f);
+    self.humanPlayer.center = CGPointMake(self.screenWidth/2.0f, self.screenHeight-30.0f);
+    self.AIPlayer.center = CGPointMake(self.screenWidth/2.0f, 30.0f);
+}
+
+-(void)viewDidLayoutSubviews{
+    [self placeObjects];
+    self.startButton.center = CGPointMake(self.screenWidth/2.0f, self.screenHeight/2.0f);
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self getScreenWidthHeight];
     self.HumanScore = 0;
     self.AIScore = 0;
+    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle]
+                                         pathForResource:@"bounce"
+                                         ofType:@"mp3"]];
+    self.audioPlayer = [[AVAudioPlayer alloc]
+                                  initWithContentsOfURL:url
+                                  error:nil];
+    //[self.audioPlayer play];
+    
+}
 
-    // Do any additional setup after loading the view.
-    //self.exitButton.hidden = TRUE;
+-(void) getScreenWidthHeight{
+    self.screenWidth = [[UIScreen mainScreen] bounds].size.width;
+    self.screenHeight = [[UIScreen mainScreen] bounds].size.height;
 }
 
 - (IBAction)startButtonClicked:(id)sender {
-    /*[self.startButton setTitleColor:[UIColor orangeColor] forState:UIControlStateNormal];*/
     self.startButton.hidden = YES;
      self.winLoseMessage.hidden = YES;
+    [self initializeBallMovement];
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(moveBall) userInfo:nil repeats:YES];
+}
+
+-(void)initializeBallMovement {
     
+    [self placeObjects];
     self.Y = arc4random() % 11;
     self.Y = self.Y-5; // gives 50% chance of moving up or down
     self.X = arc4random() % 11;
     self.X = self.X-5; // gives 50% of moving right or left
     if (self.Y == 0) {
-        self.Y = 1; // Ensures that the ball does not just move up and down
+        self.Y = 1; // Ensures that the ball does not just move left and right
     }
     if (self.X == 0) {
-        self.X = 1; // Ensures that the ball does not just move from left to right
-        
+        self.X = 1; // Ensures that the ball does not move straight up and down
     }
-    
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(moveBall) userInfo:nil repeats:YES];
 }
 
 -(void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
     UITouch *slide = [[event allTouches]anyObject];
-    self.humanPlayer.center = [slide locationInView:self.view];
-    if((self.humanPlayer.center.y > 447) || (self.humanPlayer.center.y <447)) {
-        self.humanPlayer.center = CGPointMake(self.humanPlayer.center.x, 447);
+    
+    CGPoint position = [slide locationInView:self.view];
+    
+    if(position.x > self.humanPlayer.center.x) {
+       self.humanPlayer.center = CGPointMake(self.humanPlayer.center.x+4.0f, self.humanPlayer.center.y);
+    } else if (position.x < self.humanPlayer.center.x){
+       self.humanPlayer.center = CGPointMake(self.humanPlayer.center.x-4.0f, self.humanPlayer.center.y);
     }
-    if (self.humanPlayer.center.x < 0) {
-        self.humanPlayer.center = CGPointMake(0, self.humanPlayer.center.y);
-    }
-    if(self.humanPlayer.center.x > 250) {
-        self.humanPlayer.center = CGPointMake(250, self.humanPlayer.center.y);
-    }
+    
+
+
+     if (self.humanPlayer.center.x < 35.0f) {
+        self.humanPlayer.center = CGPointMake(35.0f, self.humanPlayer.center.y);
+     }
+     if(self.humanPlayer.center.x > self.screenWidth-35.0f) {
+        self.humanPlayer.center = CGPointMake(self.screenWidth-35.0f, self.humanPlayer.center.y);
+     }
+     
 }
 
 -(void)collision {
@@ -85,10 +122,12 @@
         self.Y = arc4random() %5;
         self.Y = self.Y-5;
         // play sound
+        [self.audioPlayer play];
     }
     if(CGRectIntersectsRect(self.ball.frame, self.AIPlayer.frame)) {
         self.Y = arc4random() %5;
         // play sound
+        [self.audioPlayer play];
     }
 }
 
@@ -100,11 +139,11 @@
         self.AIPlayer.center = CGPointMake(self.AIPlayer.center.x+2, self.AIPlayer.center.y);
     }
     
-    if (self.AIPlayer.center.x < 0) {
-        self.AIPlayer.center = CGPointMake(0, self.AIPlayer.center.y);
+    if (self.AIPlayer.center.x < 35.0f) {
+        self.AIPlayer.center = CGPointMake(35.0f, self.AIPlayer.center.y);
     }
-    if(self.AIPlayer.center.x > 250) {
-        self.AIPlayer.center = CGPointMake(250, self.AIPlayer.center.y);
+    if(self.AIPlayer.center.x > self.screenWidth-35.0f) {
+        self.AIPlayer.center = CGPointMake(self.screenWidth-35.0f, self.AIPlayer.center.y);
     }
 }
 
@@ -115,24 +154,24 @@
     if(self.ball.center.x < 15){
         self.X = 0 - self.X;
         //play sound
+        [self.audioPlayer play];
     }
-    if(self.ball.center.x > 305)
+    if(self.ball.center.x > self.screenWidth -15.0f)
     {
         self.X = 0 - self.X;
         // play sound
+        [self.audioPlayer play];
     }
-   /* if (self.ball.center.y < self.view.frame.size.height) {
+    if (self.ball.center.y < 15.0f) {
         self.HumanScore += 1;
         self.humanScoreLabel.text = [NSString stringWithFormat:@"%d",self.HumanScore];
-    } else if (self.ball.center.y > self.view.frame.size.height)
+        [self initializeBallMovement];
+    } else if (self.ball.center.y > self.screenHeight-15.0f)
     {
         self.AIScore += 1;
         self.AIScoreLabel.text  = [NSString stringWithFormat:@"%d",self.AIScore];
-        //self.winLoseMessage.hidden = NO;
-       // [self.timer invalidate];
-       // self.startButton.hidden = NO;
-       // self.ball.center = CGPointMake(self.view.center.x+15, self.view.center.y+15);
-    }*/
+        [self initializeBallMovement];
+    }
     if(self.HumanScore == 5) {
         self.startButton.hidden = NO;
         [self.timer invalidate];
@@ -140,7 +179,9 @@
         self.winLoseMessage.text = [NSString stringWithFormat:@"You Win!"];
         self.HumanScore = 0;
         self.AIScore = 0;
-         // set balls and paddles back to starting position.
+        self.humanScoreLabel.text = [NSString stringWithFormat:@"%d",self.HumanScore];
+        self.AIScoreLabel.text = [NSString stringWithFormat:@"%d",self.AIScore];
+        [self placeObjects];
     } else if (self.AIScore == 5) {
         [self.timer invalidate];
         self.startButton.hidden = NO;
@@ -148,8 +189,9 @@
         self.winLoseMessage.text = [NSString stringWithFormat:@"You Lose!"];
         self.HumanScore = 0;
         self.AIScore = 0;
-        // set balls and paddles back to starting position.
-
+        self.humanScoreLabel.text = [NSString stringWithFormat:@"%d",self.HumanScore];
+        self.AIScoreLabel.text = [NSString stringWithFormat:@"%d",self.AIScore];
+        [self placeObjects];
     }
 }
 
